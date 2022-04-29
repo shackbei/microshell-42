9477aaa9e26964bb7c0de1432708f6444b23e847
ThePush <91064070+ThePush@users.noreply.github.com>
2022-04-29 10:57:32 +0200

    Update microshell.c
    improved putstr_fd2

--- microshell.c
+++ microshell.c
@@ -26,7 +26,11 @@ int	ft_putstr_fd2(char *str)
 {
 	while (*str)
 		write(2, str++, 1);
-	return(1);
+	if (arg)
+		while(*arg)
+			write(2, arg++, 1);
+	write(2, "\n", 1);
+	return (1);
 }
 
 int ft_execute(char *argv[], int i, char *env[])
@@ -35,7 +39,7 @@ int ft_execute(char *argv[], int i, char *env[])
 	//we are here in the child so it has no impact in the parent process.
 	argv[i] = NULL;
 	execve(argv[0], argv, env);
-	return (ft_putstr_fd2("error: cannot execute "), ft_putstr_fd2(argv[0]), ft_putstr_fd2("\n"));
+	return (ft_putstr_fd2("error: cannot execute ", argv[0]);
 }
 
 int	main(int argc, char *argv[], char *env[])
@@ -59,13 +63,9 @@ int	main(int argc, char *argv[], char *env[])
 		if (strcmp(argv[0], "cd") == 0) //cd
 		{
 			if (i != 2)
-				ft_putstr_fd2("error: cd: bad arguments\n");
+				ft_putstr_fd2("error: cd: bad arguments", NULL);
 			else if (chdir(argv[1]) != 0)
-			{
-				ft_putstr_fd2("error: cd: cannot change directory to ");
-				ft_putstr_fd2(argv[1]);
-				write(2, "\n", 1);
-			}
+				ft_putstr_fd2("error: cd: cannot change directory to ", argv[1);
 		}
 		else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0)) //exec in stdout
 		{
